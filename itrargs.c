387f442bdef6e0b5aab07df63f85661075346778
netoe1 <elynetobr@outlook.com>
2023-08-28 12:01:22 -0300

    adding new function

--- itrargs.c
+++ itrargs.c
@@ -25,19 +25,19 @@ static void ITRARGS_push(ITRARGS_Line *ptr, char *name)
 
         if (ptr->height == 0)
         {
-            puts("INTERNAL: push for the first time!(1/3)");
+            // puts("INTERNAL: push for the first time!(1/3)");
             ptr->string = (char **)malloc(sizeof(char) * ++ptr->height);
             ptr->string[ptr->height - 1] = (char *)malloc(sizeof(char) * BUF_SIZE);
         }
         else
         {
-            puts("INTERNAL: push more than once!(2/3)");
+            // puts("INTERNAL: push more than once!(2/3)");
             ptr->string = (char **)realloc(ptr->string, sizeof(char) * ++ptr->height);
             ptr->string[ptr->height - 1] = (char *)realloc(ptr->string[ptr->height - 1], sizeof(char) * BUF_SIZE);
         }
         strncpy(ptr->string[ptr->height - 1], name, strlen(name));
         ptr->string[strlen(name) + 1] = '\0';
-        puts("INTERNAL: string added!(3/3)");
+        // puts("INTERNAL: string added!(3/3)");
     }
 };
 static void ITRARGS_pop(ITRARGS_Line *ptr)
@@ -80,18 +80,56 @@ void ITRARGS_tokens(ITRARGS_Line *ptr, char *line)
     if (ptr != NULL)
     {
         char *buf = NULL;
-
-        buf = strtok(buffer, " ");
-        ITRARGS_push(ptr, buf);
-        ITRARGS_show(ptr);
-
-        while (buf != NULL)
+        if (strstr(line, " ") != NULL)
         {
-            buf = strtok(NULL, " ");
-            if (buf != NULL)
+            buf = strtok(buffer, " ");
+            ITRARGS_push(ptr, buf);
+            // ITRARGS_show(ptr);
+
+            while (buf != NULL)
             {
-                ITRARGS_push(ptr, buf);
+                buf = strtok(NULL, " ");
+                if (buf != NULL)
+                {
+                    ITRARGS_push(ptr, buf);
+                }
             }
+        }
+        else
+        {
+            puts("The string hasn't a empty space!");
+        }
+    }
+    // ITRARGS_show(&arguments);
+}
+
+void ITRARGS_tokens_w_pivot(ITRARGS_Line *ptr, char *line, char *pivot)
+{
+    char buffer[512];
+    strncpy(buffer, line, sizeof(buffer) - 1);
+    buffer[sizeof(buffer)] = '\0';
+    if (ptr != NULL)
+    {
+        char *buf = NULL;
+        if (pivot != NULL && strstr(line, pivot) != NULL)
+        {
+
+            buf = strtok(buffer, pivot);
+            ITRARGS_push(ptr, buf);
+            // ITRARGS_show(ptr);
+
+            while (buf != NULL)
+            {
+                buf = strtok(NULL, pivot);
+                if (buf != NULL)
+                {
+                    ITRARGS_push(ptr, buf);
+                }
+            }
+        }
+        else
+        {
+            puts("You have to set a pivot to use!");
         }
     }
     // ITRARGS_show(&arguments);
--- itrargs.h
+++ itrargs.h
@@ -16,5 +16,6 @@ void ITRARGS_init(ITRARGS_Line *ptr);
 void ITRARGS_show(ITRARGS_Line *ptr);
 void ITRARGS_free(ITRARGS_Line *ptr);
 void ITRARGS_tokens(ITRARGS_Line *ptr, char *line);
+void ITRARGS_tokens_w_pivot(ITRARGS_Line *ptr, char *line, char *pivot);
 
 #endif
